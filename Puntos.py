0c5488015690c4917be30de35db616c413631eb8
fjimalv980 <158026947+fjimalv980@users.noreply.github.com>
2024-02-29 18:59:21 +0100

    Delete Puntos.py

--- Puntos.py
+++
@@ -1,5 +0,0 @@
-# Función para recibir la cantidad de partidos ganados, empatados y perdidos y devolver los puntos conseguidos en total.
-def Puntos(resultados):
-    # Devuelvo los puntos totales que sería el resultado de multiplicar 3 por la posición 0
-    # de resultados que son las victorias y sumar la posición 2 que son los empates
-    return 3 * resultados[0] + resultados[2]
