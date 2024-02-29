d957263a72aa3108864331f8b1c9167c0445caa4
cloesinhache <158761879+cloesinhache@users.noreply.github.com>
2024-02-29 18:53:20 +0100

    segunda version

--- LeerPartidos.py
+++ LeerPartidos.py
@@ -4,30 +4,29 @@ import csv
 
 def LeerPartidos():
 
-	#guardamos en una variable la cabecera para no escribirla 8 veces
-	cabecera= ["Date","Team 1","Team 2","FT","HT"]
+    #guardamos en una variable la cabecera para no escribirla 8 veces
+    cabecera= ["Date","Team 1","Team 2","FT","HT"]
 
-	#iniciamos una lista vacia para los recoger los datos
-	lista = []
+    #iniciamos una lista vacia para los recoger los datos
+    lista = []
 
-	#usamos una excepcion por si no encontramos en fichero
-	try:
-		#abrimos el fichero y leemos su contenido con cvs reader
-		fichero = open("liga.csv")
-		contenido = csv.reader(fichero)
+    #usamos una excepcion por si no encontramos en fichero
+    try:
+        #abrimos el fichero y leemos su contenido con cvs reader
+        fichero = open("liga.csv", 'r', encoding='utf-8')
+        contenido = csv.reader(fichero)
 
-		#en un bucle for, leemos el contenido y lo vamos añadiendo a la lista
-		for dato in contenido:
-			#como queremos una lista de diccionarios, usamos la funcion zip para crearlo
-			#cabecera contiene los nombres de las columnas y dato los valores correspondientes de una fila
-			datos= dict(zip(cabecera,dato))
-			#vamos añadiendo el diccionario a la lista
-			lista.append(datos)
-		fichero.close()
+        #en un bucle for, leemos el contenido y lo vamos añadiendo a la lista
+        for dato in list(contenido)[1:]:
+            #como queremos una lista de diccionarios, usamos la funcion zip para crearlo
+            #cabecera contiene los nombres de las columnas y dato los valores correspondientes de una fila
+            datos= dict(zip(cabecera,dato))
+            #vamos añadiendo el diccionario a la lista
+            lista.append(datos)
+        fichero.close()
 
-		#la funcion nos devuelve la lista
-		return lista
+        #la funcion nos devuelve la lista
+        return lista
 
-		
-	except FileNotFoundError:
-		print("Fichero no encontrado")
+    except FileNotFoundError:
+        print("Fichero no encontrado")
